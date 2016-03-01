0f6e7d4b3a9814fb838bc6b6d5eea8c8e3cec0b1
Aidan <me@aidantaylor.net>
2016-03-01 11:48:04 +0000

    Fixed if statements

--- index.php
+++ index.php
@@ -24,23 +24,23 @@ $leds = array(7, 0, 2, 1, 3, 4, 5, 6);
 
 switch($args[0]) {
     case "led":
-        if (!in_array($args[1], $leds)) {
+        if (!isset($args[1] || !in_array($args[1], $leds)) {
             echo "LED does not exist.";
-        }
-
-        if (isset($args[2])) {
-            if ($args[2] == "on" || $args[2] == true) {
-                $value = 1;
-            } else if ($args[2] == "off" || $args[2] == false) {
-                $value = 0;
-            } else {
-                $value = $args[2];
-            }
-
-            exec("gpio mode {$args[1]} output");
-            echo exec("gpio write {$args[1]} {$value}");
         } else {
-            echo exec("gpio read {$args[1]}");
+            if (isset($args[2])) {
+                if ($args[2] == "on" || $args[2] == true) {
+                    $value = 1;
+                } else if ($args[2] == "off" || $args[2] == false) {
+                    $value = 0;
+                } else {
+                    $value = $args[2];
+                }
+
+                exec("gpio mode {$args[1]} output");
+                echo exec("gpio write {$args[1]} {$value}");
+            } else {
+                echo exec("gpio read {$args[1]}");
+            }
         }
         break;
     default:
